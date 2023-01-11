5c9b3ce6a216c520897904cbe2beb4aad7dd966d
Meghashree N <meghashreetendulkar@gmail.com>
2023-01-11 09:58:58 +0530

    Create variables.tf

---
+++ AutonomousDatabase/variables.tf
@@ -0,0 +1,9 @@
+variable "retail_autonomous_database_service_policies" {
+   type        = any
+   default = {}
+}
+
+variable "main_compartment_id"{
+   type = string
+   description = "main compartment"
+}
