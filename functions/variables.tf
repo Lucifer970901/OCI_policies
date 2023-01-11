6a7628ed0b802e585b60239076a5ee7a37c4b7fe
Meghashree N <meghashreetendulkar@gmail.com>
2023-01-11 10:01:20 +0530

    Create variables.tf

---
+++ functions/variables.tf
@@ -0,0 +1,10 @@
+variable "retail_functions_service_policies" {
+   type        = any
+   default = {}
+}
+
+
+variable "main_compartment_id"{
+   type = string
+   description = "main compartment"
+}
