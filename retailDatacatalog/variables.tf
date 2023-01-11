45f89059a5a84a357383b5797eb42fb9cf131201
Meghashree N <meghashreetendulkar@gmail.com>
2023-01-11 09:56:09 +0530

    Create variables.tf

---
+++ retailDatacatalog/variables.tf
@@ -0,0 +1,9 @@
+variable "retail_data_catalog_service_policies" {
+   type        = any
+   default = {}
+}
+
+variable "main_compartment_id"{
+   type = string
+   description = "main compartment"
+}
