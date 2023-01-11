9c3983034eccdb9d010e47bb2ce6dd90b3322192
Meghashree N <meghashreetendulkar@gmail.com>
2023-01-11 10:29:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,51 +5,51 @@ module "base_dspolcies"{
  count = length(var.datascience_policies) > 0 ? 1 : 0
  }
   
-  module "base_devopspolcies"{
+module "base_devopspolcies"{
  source              = "./devops"
  main_compartment_id = var.parent_compartment
-devops_policies = var.devops_policies
-  count = length(var.devops_policies) > 0 ? 1 : 0
+ devops_policies = var.devops_policies
+ count = length(var.devops_policies) > 0 ? 1 : 0
    }
     
-  module "base_DFpolcies"{
+module "base_DFpolcies"{
  source              = "./retailDataflow"
  main_compartment_id = var.parent_compartment
  retail_dataflow_service_policies = var.retail_dataflow_service_policies
-  count = length(var.retail_dataflow_service_policies) > 0 ? 1 : 0 
+ count = length(var.retail_dataflow_service_policies) > 0 ? 1 : 0 
    }
     
-  module "base_DCpolcies"{
- source              = "./retailDatactalog"
+module "base_DCpolcies"{
+ source              = "./retailDatacatalog"
  main_compartment_id = var.parent_compartment
-retail_data_catalog_service_policies = var.retail_data_catalog_service_policies
-  count = length(var.retail_data_catalog_service_policies) > 0 ? 1 : 0   
+ retail_data_catalog_service_policies = var.retail_data_catalog_service_policies
+ count = length(var.retail_data_catalog_service_policies) > 0 ? 1 : 0   
    }
  
-  module "base_DIpolcies"{
+module "base_DIpolcies"{
  source              = "./retaildataIntegration"
  main_compartment_id = var.parent_compartment
-retail_data_integration_service_policies = var.retail_data_integration_service_policies
-  count = length(var.retail_data_integration_service_policies) > 0 ? 1 : 0   
+ retail_data_integration_service_policies = var.retail_data_integration_service_policies
+ count = length(var.retail_data_integration_service_policies) > 0 ? 1 : 0   
    }
     
- module "base_ADBpolcies"{
- source              = "./Autonomousdatabase"
+module "base_ADBpolcies"{
+ source              = "./AutonomousDatabase"
  main_compartment_id = var.parent_compartment
-  retail_autonomous_database_service_policies = var.retail_autonomous_database_service_policies
-  count = length(var.retail_autonomous_database_service_policies) > 0 ? 1 : 0   
+ retail_autonomous_database_service_policies = var.retail_autonomous_database_service_policies
+ count = length(var.retail_autonomous_database_service_policies) > 0 ? 1 : 0   
    }
    
-  module "base_AIpolcies"{
+module "base_AIpolcies"{
  source              = "./AI"
  main_compartment_id = var.parent_compartment
  retail_ai_service_policies = var.retail_ai_service_policies
   count8 = length(var.retail_ai_service_policies) > 0 ? 1 : 0   
    }
     
- module "base_Funpolcies"{
+module "base_Funpolcies"{
  source              = "./functions"
  main_compartment_id = var.parent_compartment
-retail_functions_service_policies = var.retail_functions_service_policies
+ retail_functions_service_policies = var.retail_functions_service_policies
  count = length(var.retail_functions_service_policies) > 0 ? 1 : 0   
  }
